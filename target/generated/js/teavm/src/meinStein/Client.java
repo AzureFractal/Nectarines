2b39e6d4d08547b9b2aefcc9bf7d0736b026b6be
Yong Hui Lim <30565867+AzureFractal@users.noreply.github.com>
2023-10-07 17:42:08 -0400

    Initial Commit

---
+++ target/generated/js/teavm/src/meinStein/Client.java
@@ -0,0 +1,19 @@
+package meinStein;
+
+import org.teavm.jso.dom.html.HTMLDocument;
+
+public class Client {
+    private static MeinCtrl meinCtrl;
+
+    public static void main(String[] args) {
+//        meinDisplay = new MeinDisplay();
+        meinCtrl = new MeinCtrl();
+        meinCtrl.resetBoard();
+        System.out.println("Okay it work?");
+
+        var document = HTMLDocument.current();
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode("TeaVM generated element"));
+        document.getBody().appendChild(div);
+    }
+}
