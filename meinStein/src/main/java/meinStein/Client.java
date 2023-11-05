2f8fd4afce724b2d5b60f955c59b0e04fb73a3e2
Yong Hui Lim <30565867+AzureFractal@users.noreply.github.com>
2023-11-05 15:52:55 -0500

    Update communication for multipwe pwayer moweves

--- meinStein/src/main/java/meinStein/Client.java
+++ meinStein/src/main/java/meinStein/Client.java
@@ -15,46 +15,58 @@ public class Client {
     public static int readCommsAndPlayMove(HTMLDocument document) {
         document.getElementById("comms1").setInnerHTML("Thinking");
         String commsStr = document.getElementById("comms").getInnerText();
-        String[] userMoves = commsStr.split(",");
-        // Play user moves from buffer
-        if (userMoves.length > 1) {
-            int stone1 =  Integer.parseInt(userMoves[userMoves.length-2].split(":")[0]);
-            int stone2 =  Integer.parseInt(userMoves[userMoves.length-1].split(":")[0]);
+
+        // Backtrack to latest common ancestor
+        while (!meinCtrl.curGame.getMovesEncoded().equals(commsStr) && meinCtrl.curGame.ply > 1) {
+            meinCtrl.takeBack(0);
+        }
+
+        // Extract new moves
+        String[] newMoves = commsStr.substring(meinCtrl.curGame.getMovesEncoded().length()).split(",");
+
+        // Do length - 1 here because there is a trailing ","
+        // Play new moves
+        for (int i = 0; i < newMoves.length - 1; i+=2) {
+            int stone1 =  Integer.parseInt(newMoves[i].split(":")[0]);
+            int stone2 =  Integer.parseInt(newMoves[i+1].split(":")[0]);
 
             if (meinCtrl.cur.num[stone1] == 0 && meinCtrl.cur.num[stone2] == 0) {
-                int col = ((userMoves.length - 1) / 2) % 2 == 0 ? MeinCtrl.black : MeinCtrl.white;
-                // This chunk must be run before actually playing the move, otherwise evalTwoSq will erase the move
-                MeinCtrl.Move combinedMove = meinCtrl.cur.evalTwoSq(col, stone1, stone2);
-                for (int i = 0; i < NUM_FEAT; i++) {
-                    if (col == MeinCtrl.black) {
-                        MeinCtrl.runningFeatCountBlack[i] += combinedMove.featCountPlayer[i];
-                        MeinCtrl.runningFeatCountWhite[i] += combinedMove.featCountOpp[i];
-                    } else {
-                        MeinCtrl.runningFeatCountWhite[i] += combinedMove.featCountPlayer[i];
-                        MeinCtrl.runningFeatCountBlack[i] += combinedMove.featCountOpp[i];
-                    }
-
-                    System.out.println(MeinCtrl.chainType[i] + ":" + MeinCtrl.runningFeatCountBlack[i] + "," + MeinCtrl.runningFeatCountWhite[i]);
-                }
+//                int col = ((newMoves.length - 1) / 2) % 2 == 0 ? MeinCtrl.black : MeinCtrl.white;
+//                // This chunk must be run before actually playing the move, otherwise evalTwoSq will erase the move
+//                MeinCtrl.Move combinedMove = meinCtrl.cur.evalTwoSq(col, stone1, stone2);
+//                for (int j = 0; j < NUM_FEAT; j++) {
+//                    if (col == MeinCtrl.black) {
+//                        MeinCtrl.runningFeatCountBlack[j] += combinedMove.featCountPlayer[j];
+//                        MeinCtrl.runningFeatCountWhite[j] += combinedMove.featCountOpp[j];
+//                    } else {
+//                        MeinCtrl.runningFeatCountWhite[j] += combinedMove.featCountPlayer[j];
+//                        MeinCtrl.runningFeatCountBlack[j] += combinedMove.featCountOpp[j];
+//                    }
+//
+//                    System.out.println(MeinCtrl.chainType[j] + ":" + MeinCtrl.runningFeatCountBlack[j] + "," + MeinCtrl.runningFeatCountWhite[j]);
+//                }
 
                 meinCtrl.tryMove(stone1, stone2);
             }
         }
+
         System.out.println("Depth qDepth:" + depth + "," + quietDepth);
         // Get computer moves and update buffer
         int score = meinCtrl.cur.anaPlay(depth, quietDepth);
         document.getElementById("comms").setInnerHTML(meinCtrl.curGame.getMovesEncoded());
         document.getElementById("comms1").setInnerHTML("Done");
         // For now we need a negative sign here because the score is for the AI
-        document.getElementById("scoreText").setInnerHTML("Score:" + (-score) + " (+ve is good 4 u u r happy and healthy) <br> Your win prob:" + Math.round((1.0 / (1.0+Math.exp(-0.001*-score)))*100)  + "%");
+        document.getElementById("scoreText").setInnerHTML("Score:" + (-score) +
+                " (+ve is good 4 u u r happy and healthy) <br> Your win prob:" +
+                Math.round((1.0 / (1.0+Math.exp(-0.001*-score)))*100)  + "%");
 
-        String chainCountText = "";
-        for (int i = 1; i < NUM_FEAT; i++) {
-            chainCountText += MeinCtrl.chainType[i] + ":";
-            chainCountText += MeinCtrl.runningFeatCountBlack[i] + ",";
-            chainCountText += MeinCtrl.runningFeatCountWhite[i] + "\n";
-        }
-        document.getElementById("chainCount").setInnerHTML(chainCountText);
+//        String chainCountText = "";
+//        for (int i = 1; i < NUM_FEAT; i++) {
+//            chainCountText += MeinCtrl.chainType[i] + ":";
+//            chainCountText += MeinCtrl.runningFeatCountBlack[i] + ",";
+//            chainCountText += MeinCtrl.runningFeatCountWhite[i] + "\n";
+//        }
+//        document.getElementById("chainCount").setInnerHTML(chainCountText);
 
 
         if (-MeinCtrl.posVal[MeinCtrl.DONE6] == score){
